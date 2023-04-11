40ff5f59a7bbbe67b582c25fd4fd248d0321a50d
Micah Sharifi <sharifimicah17@gmail.com>
2023-04-10 21:11:10 -0700

    start code team presenter

---
+++ __tests__/Engineer.jest.js
@@ -0,0 +1,37 @@
+* `name`
+
+* `id`
+
+* `email`
+
+* `getName()`
+
+* `getId()`
+
+* `getEmail()`
+
+* `getRole()`&mdash;returns `'Employee'`
+
+The other three classes will extend `Employee`.
+
+In addition to `Employee`'s properties and methods, `Manager` will also have the following:
+
+* `officeNumber`
+
+* `getRole()`&mdash;overridden to return `'Manager'`
+
+In addition to `Employee`'s properties and methods, `Engineer` will also have the following:
+
+* `github`&mdash;GitHub username
+
+* `getGithub()`
+
+* `getRole()`&mdash;overridden to return `'Engineer'`
+
+In addition to `Employee`'s properties and methods, `Intern` will also have the following:
+
+* `school`
+
+* `getSchool()`
+
+* `getRole()`&mdash;overridden to return `'Intern'`
